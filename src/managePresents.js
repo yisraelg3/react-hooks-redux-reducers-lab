cf7f22d731f18ab8fd75299b4d8e6201329a5681
Yisrael Gurkow <yisrelg3@gmail.com>
2021-07-18 15:05:42 -0400

    commit tree 98087f5b50df09a746512302375c9147fa572e6c

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,18 @@
-export function managePresents(state, action) {
-  // your code here
+export function managePresents(state={numberOfPresents: 0}, action) {
+  switch (action.type) {
+    case "presents/increase":
+      return {numberOfPresents: state.numberOfPresents + 1}
+  }
 }
+
+
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case "counter/increment":
+//       return { count: state.count + 1 };
+//     case "counter/decrement":
+//       return { count: state.count - 1 };
+//     default:
+//       return state;
+//   }
+// } 
